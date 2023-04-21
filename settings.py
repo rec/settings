46879e3c9c261b9d8ef5153fa29209667ca1def5
Tom Ritchford <tom@swirly.com>
2023-04-21 14:33:30 +0200

    Add a unit test

--- settings.py
+++ settings.py
@@ -1,40 +1,38 @@
-import dataclasses
 from enum import Enum
+from pathlib import Path
+from typing import Any, Dict, Optional, Tuple, Union
+import json
+import os
+import sys
+
+File = Tuple[Union[Path, str]]
+_NONE = object()
 
 
 class Settings:
-    _attribs_set = None
+    _settings_modified = None
 
     def __post_init__(self):
-        self._attribs_set = set()
+        self._settings_modified = set()
 
-    def __setattr__(self, k, v):
-        if self._attribs_set is None:
+    def __setattr__(self, k: str, v: Any):
+        if self._settings_modified is None:  # During construction
             super().__setattr__(k, v)
         else:
-            previous = getattr(self, k)
-            if isinstance(previous, Enum) and isinstance(v, str):
-                v = type(previous)(v)
-
             self.__dict__[k] = v
-            self._attribs_set.add(k)
+            self._settings_modified.add(k)
 
-    def changed_dict(self):
-        result = {}
-        try:
-            d = self.asdict()
-        except Exception:
-            d = dataclasses.asdict(self)
-
-        for k, v in d.items():
-            if isinstance(v, Settings):
-                d = v.asdict()
+    def modified(self):
+        results = {}
+        for k, v in vars(self).items():
+            if k in self._settings_modified:
+                results[k] = v
+            elif isinstance(v, Settings) and not k.startswith('_'):
+                d = v.modified()
                 if d:
-                    result[k] = d
-            elif k in self._attribs_set:
-                result[k] = v
+                    results[k] = d
 
-        return result
+        return results
 
     def copy_from(self, **kwargs):
         for k, v in kwargs.items():
@@ -43,3 +41,87 @@ class Settings:
                 attr.copy_from(**v)
             else:
                 setattr(self, k, v)
+
+    def load(self, *files: File):
+        for f in files:
+            self.copy_from(_load(f))
+
+    def load_from_environ(
+        self,
+        prefix: str,
+        environ: Optional[Dict] = None,
+        verbose: bool = True,
+    ):
+        if environ is None:
+            environ = os.environ
+        pre = prefix.strip('_').upper() + '_'
+        items = sorted(environ.items())
+        items = ((k, v) for k, v in items if k.startswith(pre))
+
+        for k, v in items:
+            attr_name = k[len(pre):].lower()
+            splits = list(_split_address(v, attr_name))
+            if len(splits) == 1:
+                parent, attr = splits[0]
+                str_val = getattr(parent, attr)
+                val = _string_value(k, v, str_val)
+                setattr(parent, attr, val)
+            elif not verbose:
+                continue
+            elif not splits:
+                print('No settings match', k, file=sys.err)
+            else:
+                print('More than one setting matches', k, file=sys.err)
+
+
+def _split_address(parent, key):
+    if key in dir(parent):
+        yield parent, key
+    else:
+        for k in dir(parent):
+            if k.startswith(key + '_'):
+                k = key[len(key) + 1:]
+                new_parent = getattr(parent, k)
+                yield from _split_address(new_parent, k)
+
+
+def _string_value(name, v, original_value):
+    if original_value is None or isinstance(v, str):
+        return v
+
+    if isinstance(original_value, int):
+        return int(v)
+
+    if isinstance(original_value, float):
+        return float(v)
+
+    if isinstance(original_value, bool):
+        if v.lower() in ('t', 'true'):
+            return True
+        if v.lower() in ('f', 'false'):
+            return False
+        raise ValueError(f'Cannot understand bool {name}={v}')
+
+    if isinstance(original_value, Enum):
+        return type(original_value)[v]
+
+    return json.loads(v)
+
+
+def _load(p):
+    if p.suffix == '.json':
+        return json.loads(p.read_text())
+
+    if p.suffix == '.toml':
+        try:
+            import tomllib
+            return tomllib.loads(p.read_text())
+        except ImportError:
+            import tomlkit
+            return tomlkit.loads(p.read_text())
+
+    if p.suffix == '.yaml':
+        import yaml
+        return yaml.safe_load(p.read_text())
+
+    raise ValueError(f'Do not understand {p.suffix=}')
---
+++ test_settings.py
@@ -0,0 +1,49 @@
+import dataclasses as dc
+from settings import Settings
+from typing import List
+
+def field(factory):
+    return dc.field(default_factory=factory)
+
+
+@dc.dataclass
+class Audio(Settings):
+    levels: List[float] = field(list)
+
+
+@dc.dataclass
+class DMX(Settings):
+    channel: int = 0
+
+
+@dc.dataclass
+class Midi(Settings):
+    channel: int = 0
+    name: str = ''
+
+
+@dc.dataclass
+class Everything(Settings):
+    audio: Audio = field(Audio)
+    dmx: DMX = field(DMX)
+    midi: Midi = field(Midi)
+
+
+def test_simple():
+    e = Everything()
+    assert e.modified() == {}
+
+    e.dmx.channel = 3
+    assert e.modified() == {'dmx': {'channel': 3}}
+
+
+def test_lists():
+    e = Everything()
+    assert e.modified() == {}
+
+    e.audio.levels[:] = [1.0, 2.0]
+    assert e.modified() == {}
+    # Can't detect this
+
+    e.audio.levels = [1.0, 2.0]
+    assert e.modified() == {'audio': {'levels': [1.0, 2.0]}}
